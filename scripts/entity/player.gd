8a1c9c96ed6f45c20a5bcb3d9d596bb5f78b8638
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-09-21 17:59:53 +0200

    fix: xp

--- scripts/entity/player.gd
+++ scripts/entity/player.gd
@@ -83,4 +83,6 @@ func _physics_process(delta: float) -> void:
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group('enemy'):
 		PlayerStats.speed = min(PlayerStats.speed + PlayerStats.acceleration, PlayerStats.maxSpeed)
+		actualXp += area.get_parent().xpGain
+		print(actualXp)
 		area.get_parent().HEALTH -= 1
