9519ef79de609c3cb2bf26c46e22dfef0ce00b0d
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-09-24 18:35:57 +0200

    feat: UI + mod spawn
    - make mob spawn near player for first 10 seconds
    - center main menu play button
    - anchor speedometer

--- scripts/globals/Globals.gd
+++ scripts/globals/Globals.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node
 
 const COMMON_CARDS = ["res://scenes/upgradeCards/common/accelerationUp.tscn", "res://scenes/upgradeCards/common/damageUp.tscn", "res://scenes/upgradeCards/common/maxSpeed.tscn", "res://scenes/upgradeCards/common/momentumUp.tscn", "res://scenes/upgradeCards/common/rotationSpeedUp.tscn", "res://scenes/upgradeCards/common/sizeUp.tscn"]
 const RARE_CARDS = ["res://scenes/upgradeCards/rare/accelerationUp.tscn", "res://scenes/upgradeCards/rare/damageUp.tscn", "res://scenes/upgradeCards/rare/maxSpeed.tscn", "res://scenes/upgradeCards/rare/momentumUp.tscn", "res://scenes/upgradeCards/rare/rotationSpeedUp.tscn", "res://scenes/upgradeCards/rare/sizeUp.tscn"]
@@ -7,3 +7,12 @@ const LEGENDARY_CARDS = ["res://scenes/upgradeCards/legendary/accelerationUp.tsc
 var cameraPos: Vector2
 
 var worldSize: Vector2i
+
+var game_start_time: int = 0
+
+func set_game_start_time():
+	game_start_time = Time.get_ticks_msec()
+
+# get elapsed time in seconds
+func get_game_elapsed_time():
+	return (Time.get_ticks_msec() - game_start_time) / 1000.0
