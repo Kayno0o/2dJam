0be2bbfc834dc2c54954418f7d34e9df34b7cb5a
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-09-24 20:27:23 +0200

    feat: box explosion particles

--- scripts/prop/box.gd
+++ scripts/prop/box.gd
@@ -1,14 +1,23 @@
 extends Area2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+@onready var particles_scene = preload("res://scenes/particles/box_explosion.tscn")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D && body.is_in_group("player"):
+		var player: CharacterBody2D = body
 		PlayerStats.speed = min(PlayerStats.speed - PlayerStats.acceleration, PlayerStats.maxSpeed)
+
+		# spawn explosion particles in front of the player
+		var particles: GPUParticles2D = particles_scene.instantiate()
+		player.add_child(particles)
+
+		var normalized_velocity = player.velocity.normalized()
+		
+		var particleMaterial: ParticleProcessMaterial = particles.process_material
+		if particleMaterial is ParticleProcessMaterial:
+			# offset particles spawn
+			particleMaterial.emission_shape_offset = Vector3(abs(normalized_velocity.x) * 32, 0, 0)
+
+		particles.emitting = true
+
 		queue_free()
