03bf9545956fc9150853e346d1fef44eb594a10d
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-09-25 19:23:57 +0200

    refactor: move mob death function

--- scripts/entity/enemy.gd
+++ scripts/entity/enemy.gd
@@ -17,19 +17,6 @@ func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 	health = 1 + floor(Globals.get_game_elapsed_time() / 25.0)
 
-func _process(_delta: float) -> void:
-	if health <= 0:
-		PlayerStats.xp += xpGain
-		PlayerStats.speed = min(PlayerStats.speed + PlayerStats.acceleration, PlayerStats.maxSpeed)
-
-		var particles: GPUParticles2D = particles_scene.instantiate()
-		particles.position = position
-
-		get_parent().add_child(particles)
-		particles.emitting = true
-
-		queue_free()
-
 func _physics_process(delta: float) -> void:
 	# make the enemy goal position to the player if far enough
 	if position.distance_to(player.position) > MAX_DISTANCE:
@@ -48,3 +35,15 @@ func _physics_process(delta: float) -> void:
 func _on_hurtbox_area_entered(area: Area2D) -> void:
 	if area is Area2D && area.get_parent().is_in_group("player"):
 		health -= PlayerStats.damage
+
+		if health <= 0:
+			PlayerStats.xp += xpGain
+			PlayerStats.speed = min(PlayerStats.speed + PlayerStats.acceleration, PlayerStats.maxSpeed)
+
+			var particles: GPUParticles2D = particles_scene.instantiate()
+			particles.position = position
+
+			get_parent().add_child(particles)
+			particles.emitting = true
+
+			queue_free()
