3b66dee89f4e70d8bb96e0e21aae80a8b37b2263
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-09-25 21:33:42 +0200

    feat: add enemy health bar

--- scripts/entity/enemy.gd
+++ scripts/entity/enemy.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
+@export var progress_bar: TextureProgressBar
+
 var particles_scene = preload("res://scenes/particles/mob_kill.tscn")
 
 var player
+var max_health = 1.0
 var health = 1.0
 
 const SPEED = 5000
@@ -15,7 +18,11 @@ var is_move_toward = false
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
+
 	health = 1 + floor(Globals.get_game_elapsed_time() / 25.0)
+	max_health = health
+
+	progress_bar.visible = false
 
 func _physics_process(delta: float) -> void:
 	# make the enemy goal position to the player if far enough
@@ -35,6 +42,10 @@ func _physics_process(delta: float) -> void:
 func _on_hurtbox_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D and body.is_in_group("player"):
 		health -= PlayerStats.damage
+
+		progress_bar.max_value = max_health
+		progress_bar.value = health
+		progress_bar.visible = true
 
 		var particles: GPUParticles2D = particles_scene.instantiate()
 		particles.position = position
