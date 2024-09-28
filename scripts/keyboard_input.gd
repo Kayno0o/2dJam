d5311c8d048f975e817c24c617b7c1202dd04f33
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-09-28 11:53:01 +0200

    fix: do not allow to pause/unpause when item shop is open

--- scripts/keyboard_input.gd
+++ scripts/keyboard_input.gd
@@ -3,5 +3,6 @@ extends Control
 func _input(event: InputEvent) -> void:
 	# pauses when pressing P
 	if event is InputEventKey:
-		if event.keycode == 80 and event.is_pressed() == true:
+		if event.keycode == 80 and event.is_pressed() == true and not Game.is_shop_open:
 			get_tree().paused = !get_tree().paused
+			Game.is_paused = get_tree().paused
