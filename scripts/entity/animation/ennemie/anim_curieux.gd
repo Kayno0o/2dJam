07d99a603000d899db4930dcde4338a5ea54ba9b
Pediluve <atoutain@student.42.fr>
2024-09-28 04:01:35 +0200

    fix : scripts
    -detached archer spritecontroller wrong script and corrected back the Curieux animation script

--- scripts/entity/animation/ennemie/anim_curieux.gd
+++ scripts/entity/animation/ennemie/anim_curieux.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 func _on_curieux_moving():
 	$Idle.visible = true
-	$Shooting.visible = false
+	$Scared.visible = false
 
 func _on_curieux_scared():
 	$Idle.visible = false
-	$Shooting.visible = true
+	$Scared.visible = true
